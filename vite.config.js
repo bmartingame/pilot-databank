6cc9e9bd26437d8549da178abe3c6695fbae3fda
bmartingame <54820140+bmartingame@users.noreply.github.com>
2026-07-11 08:49:11 -0400

    Remove comments from vite.config.js
    Removed comments about relative assets in config.

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-
-  // Relative assets allow the same build to work at:
-  // username.github.io/repository-name/
-  // or at a custom domain.
   base: "./",
 });
